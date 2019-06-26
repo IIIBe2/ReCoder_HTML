354f13875bc87b01fc71a8ff559dd1ca25248375
IIIBe2 <42515284+IIIBe2@users.noreply.github.com>
2019-06-26 13:57:01 +0900

    Add files via upload

---
+++ StartProg.bat
@@ -0,0 +1,4 @@
+EnterCodeSimvol.txt
+python main.py
+ModOutCodeSimvol.txt
+start https://snipp.ru/tools/ascii
---
+++ main.py
@@ -0,0 +1,47 @@
+import random
+Of = open("EnterCodeSimvol.txt","r")
+Wf = open("OutCodeSimvol.txt","w")
+Mf = open("ModOutCodeSimvol.txt","w")
+
+def Mod():
+  Mf.write(listAlpha.get(Temp))
+  if Temp != ' ':
+    Mf.write('&#' + str(random.randint(768, 879)) + ';')
+    Mf.write('&#' + str(random.randint(768, 879)) + ';')
+    Mf.write('&#' + str(random.randint(768, 879)) + ';')
+    Mf.write('&#' + str(random.randint(768, 879)) + ';')
+    Mf.write('&#' + str(random.randint(768, 879)) + ';') 
+    Mf.write('&#' + str(random.randint(768, 879)) + ';')
+    Mf.write('&#' + str(random.randint(768, 879)) + ';')
+    Mf.write('&#' + str(random.randint(768, 879)) + ';')
+    Mf.write('&#' + str(random.randint(768, 879)) + ';')
+    Mf.write('&#' + str(random.randint(768, 879)) + ';') 
+    Mf.write('&#' + str(random.randint(768, 879)) + ';')
+    Mf.write('&#' + str(random.randint(768, 879)) + ';')
+    Mf.write('&#' + str(random.randint(768, 879)) + ';')
+    Mf.write('&#' + str(random.randint(768, 879)) + ';')
+    Mf.write('&#' + str(random.randint(768, 879)) + ';') 
+
+listAlpha = {'A':'&#65;','B':'&#66;','C':'&#67;','D':'&#68;','E':'&#69;','F':'&#70;','G':'&#71;',
+             'H':'&#72;','I':'&#73;','J':'&#74;','K':'&#75;','L':'&#76;','M':'&#77;','N':'&#78;',
+             'O':'&#79;','P':'&#80;','Q':'&#81;','R':'&#82;','S':'&#83;','T':'&#84;','U':'&#85;',
+             'V':'&#86;','W':'&#87;','X':'&#88;','Y':'&#89;','Z':'&#90;','a':'&#97;', 'b':'&#98;',
+             'c':'&#99;','d':'&#100;','e':'&#101;','f':'&#102;','g':'&#103;','h':'&#104;','i':'&#105;',
+             'j':'&#106;','k':'&#107;','l':'&#108;','m':'&#109;','n':'&#110;','o':'&#111;','p':'&#112;',
+             'q':'&#113;','r':'&#114;','s':'&#115;','t':'&#116;','u':'&#117;','v':'&#118;','w':'&#119;',
+             'x':'&#120;','y':'&#121;','z':'&#122;', ' ':'&#32;'}
+
+Temp = Of.read(1)
+while Temp:
+  if Temp in listAlpha: 
+    Wf.write(listAlpha.get(Temp))
+    Mod()
+  Temp = Of.read(1)
+
+with open('OutCodeSimvol.txt', 'wb'):
+    with open('ModOutCodeSimvol.txt', 'wb'):
+        with open('EnterCodeSimvol.txt', 'wb'):
+          pass
+
+Wf.close()
+Of.close()
